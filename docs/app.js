6aeec6d0605f5964cc5fa7f221b365d171a0423f
ezzone72 <33310686+ezzone72@users.noreply.github.com>
2026-02-01 22:29:41 +0900

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -1,9 +1,9 @@
-// PWA Service Worker (shell 단계)
-
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
-});
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAGpSq-UbQW3HLWBPsYC8q0tz3DQPNBIjI",
+  authDomain: "fx-alert-73663.firebaseapp.com",
+  projectId: "fx-alert-73663",
+  storageBucket: "fx-alert-73663.firebasestorage.app",
+  messagingSenderId: "4733193042",
+  appId: "1:4733193042:web:7c3853ed9e77de97fd488b"
+};
